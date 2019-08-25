e35f16c47964c1d6a38b35a73cd5e5e89fffbfac
Joseph DeCommer <jdecomer@gmail.com>
2019-08-25 15:50:08 -0400

    init

---
+++ init.sql
@@ -0,0 +1,28 @@
+CREATE TABLE pooquotes (
+  ID SERIAL PRIMARY KEY,
+  quote VARCHAR(255) NOT NULL
+);
+
+INSERT INTO pooquotes (quote) VALUES ('This is the first poo of the New Year!');
+INSERT INTO pooquotes (quote) VALUES ('The number two poo');
+INSERT INTO pooquotes (quote) VALUES ('My bowels rested on the 3rd day of the year, but not the 4th!');
+INSERT INTO pooquotes (quote) VALUES ('I call this one "The Knot');
+INSERT INTO pooquotes (quote) VALUES ('Disregard the hair. GROSS!');
+INSERT INTO pooquotes (quote) VALUES ('The Murky deep.');
+INSERT INTO pooquotes (quote) VALUES ('This poo broke up on contact.');
+INSERT INTO pooquotes (quote) VALUES ('I don''t know why this one is so red. Ketchup perhaps?');
+INSERT INTO pooquotes (quote) VALUES ('First of the many "goo" poos');
+INSERT INTO pooquotes (quote) VALUES ('Explosion!');
+INSERT INTO pooquotes (quote) VALUES ('Hey corn!! It IS real!');
+INSERT INTO pooquotes (quote) VALUES ('This turd is looking a little grey');
+INSERT INTO pooquotes (quote) VALUES ('I like the ropiness of this one.');
+INSERT INTO pooquotes (quote) VALUES ('Small and not too spectacular.');
+INSERT INTO pooquotes (quote) VALUES ('It''s a poo!');
+INSERT INTO pooquotes (quote) VALUES ('Lumpy in the front');
+INSERT INTO pooquotes (quote) VALUES ('Pardon the dark appearance of this sea monster');
+INSERT INTO pooquotes (quote) VALUES ('This one is a pretty big mound');
+INSERT INTO pooquotes (quote) VALUES ('I was too concerned with wiping my butt to make a quote I guess');
+INSERT INTO pooquotes (quote) VALUES ('Sooper loong!');
+INSERT INTO pooquotes (quote) VALUES ('Nice coil to this one');
+INSERT INTO pooquotes (quote) VALUES ('This girth is something to behold!');
+INSERT INTO pooquotes (quote) VALUES ('The longest poo to close out January.');
